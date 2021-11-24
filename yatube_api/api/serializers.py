af50c74899cf8ee4f42216736d3d6b3702a48a5f
Vladimir <khodykin.volodymyr@yandex.ru>
2021-11-24 19:37:18 +0300

    make api on viewsets

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.generics import get_object_or_404
 
+from posts.models import Comment, Post, Follow, Group, User
 
-from posts.models import Comment, Post
+CANT_SUBSCRIBE_URSELF = 'Нельзя подписаться самому на себя!'
+ALREADY_FOLLOW = 'Вы уже подписаны на этого автора.'
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -13,6 +16,13 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
 
 
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
@@ -21,3 +31,31 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    def validate(self, data):
+        user = get_object_or_404(User, username=data['following'].username)
+        follow = Follow.objects.filter(
+            user=self.context['request'].user,
+            following=user
+        )
+        if user == self.context['request'].user:
+            raise serializers.ValidationError(CANT_SUBSCRIBE_URSELF)
+        if follow:
+            raise serializers.ValidationError(ALREADY_FOLLOW)
+        return data
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        read_only_fields = ('id', 'user')
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,59 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets, mixins, filters
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
+
+from posts.models import Post, Group
+
+from .serializers import (GroupSerializer, PostSerializer,
+                          CommentSerializer, FollowSerializer)
+from .permissions import AuthorReadOnlyAll
+
+
+class RetrieveModelMixin(mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                         viewsets.GenericViewSet):
+    pass
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AuthorReadOnlyAll,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(RetrieveModelMixin):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (AuthorReadOnlyAll,)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorReadOnlyAll,)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        return post.comments
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        serializer.save(author=self.request.user, post=post)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthenticated, )
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['=user__username', '=following__username', ]
+
+    def get_queryset(self):
+        return self.request.user.follower.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
